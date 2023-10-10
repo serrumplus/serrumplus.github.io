62384484d729c97137ac00d6d74479a2f52aef49
Your Name <you@example.com>
2023-10-10 16:59:11 +0300

    delete

--- scripts/pixels/facebook/fbpxviewcontenttime.js
+++
@@ -1,7 +0,0 @@
-<script>
-// Delay pixel fire
-var fbseconds = {SECONDS};
-setTimeout(function() {
-  fbq('track', 'ViewContent',{content_name:'{PAGE}',content_category:'Time'});
-}, fbseconds * 1000);
-</script>
